0461079f7f2df94e78a6e50f10e8718be7040651
Kelvi3 <theocazenavecoupet@gmail.com>
2021-06-07 13:21:11 +0200

    UPDATE: home for intro

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -14,17 +14,19 @@ const Home = () => {
     console.log(annoucements)
 
     return (
-        <div>
-            <h1>Home</h1>
-            {annoucements.map(annoucement => 
-            <div key={annoucement.id} className="annoucement">
-                <p>{annoucement.title}</p>
-                <p>{annoucement.description}</p>
-                <p>{annoucement.price}</p>
-                <p>{annoucement.size} m2</p>
+        <>
+            <div className="intro">
+                <p className="intro-content">Immobilier.com est un site sur le quel vous pouvez trouver votre appartement ou maison au meuilleur prix</p>
             </div>
+            {annoucements.map(annoucement => 
+                <div key={annoucement.id} className="annoucement">
+                    <p>{annoucement.title}</p>
+                    <p>{annoucement.description}</p>
+                    <p>{annoucement.price}</p>
+                    <p>{annoucement.size} m2</p>
+                </div>
             )}             
-        </div>
+        </>
     )
 }
 
