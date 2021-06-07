2aa48c7dba4c9d19854d0ac2d789589b17f62069
Kelvi3 <theocazenavecoupet@gmail.com>
2021-06-07 13:05:05 +0200

    ADD: anoucement in home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,30 @@
-import React from "react"
+import React, {useState ,useEffect } from "react"
+import './home.css'
 
-const Home =()=>{
+const Home = () => {
+
+    const [annoucements, setAnnoucements] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/annoucements')
+            .then((response) => response.json())
+            .then((response) =>  setAnnoucements(response))
+    }, [])
+
+    console.log(annoucements)
 
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            {annoucements.map(annoucement => 
+            <div key={annoucement.id} className="annoucement">
+                <p>{annoucement.title}</p>
+                <p>{annoucement.description}</p>
+                <p>{annoucement.price}</p>
+                <p>{annoucement.size} m2</p>
+            </div>
+            )}             
+        </div>
     )
 }
 
