a96a8bd1946dbd845375fe0201b0dc7aac6f52e8
Boris <seikenfr@yahoo.fr>
2021-06-08 15:18:54 +0200

    added: pitures appart/home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,8 +8,12 @@ const Home = () => {
     
     const [annoucements, setAnnoucements] = useState([])
     const [city, setCity] = useState('')
-
     const [data, setData] = useState([])
+    const picHouses = ['https://images.unsplash.com/photo-1567428485548-c499e4931c10?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
+    'https://images.unsplash.com/photo-1565721219423-c8cd193201c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+    ]
+
+    
    
     useEffect(() => {
         fetch('http://localhost:3000/annoucements')
@@ -61,11 +65,17 @@ const Home = () => {
             {annoucements.map(annoucement => 
                 <div key={annoucement.id} className="annoucement">
                     <div className="container">
-                        <h3>{annoucement.title}</h3>
-                        <p>{annoucement.description}</p>
-                        <p>{annoucement.city}</p>
-                        <p>{annoucement.price}</p>
-                        <p>{annoucement.size} m2</p>
+                        {annoucement.typeHome === "Maison"?(
+                            <img className='img-type' src={picHouses[0]} alt="maison" />
+                        ):(<img className='img-type' src={picHouses[1]} alt="Appartement" />)}
+                        <div>
+                            <h3>{annoucement.title}</h3>
+                            <p>{annoucement.description}</p>
+                            <p>{annoucement.city}</p>
+                            <p>{annoucement.typeHome}</p>
+                            <p>{annoucement.price} €</p>
+                            <p>{annoucement.size} m2</p>
+                        </div>
                         <Link to={`/showAnnoucement/${annoucement.id}`}><button className="btn">Voir l'annonce</button></Link>
                     </div>
                 </div>
