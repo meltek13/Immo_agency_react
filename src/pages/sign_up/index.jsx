7d971327de5e54effbcb663e0e1016ec309db145
Boris <seikenfr@yahoo.fr>
2021-06-09 14:35:59 +0200

    added: img view signin

--- src/pages/sign_up/index.jsx
+++ src/pages/sign_up/index.jsx
@@ -4,7 +4,9 @@ import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import {useDispatch } from "react-redux";
 import { logIn } from "store-redux/index";
-import './sign_up.css'
+import './sign_up.css';
+import signin_svg from 'assets/img/signin.svg';
+
 
 const SignUp = () => {
  
@@ -52,6 +54,8 @@ const SignUp = () => {
   return (
     <>
     <div className="Register">
+    <h2 className="h2-signin">Créer un compte</h2>
+      <img className='svg-signin' src={signin_svg} alt="illustration" />
         <form>
         <input className="input-log" type="Email" name="Email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <input className="input-log" type="Password" name="Password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} />
